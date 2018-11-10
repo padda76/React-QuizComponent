7fbfbf85723ba0fc1920fc98be2c6dd02428efb7
padda76 <odd.hartvigsen@gmail.com>
2018-11-10 12:07:55 +0100

    #6 errorMsg

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            incorrectAnswer: false
+        };
+    }
 
     handleClick(buttonText){
+        let incorrectAnswer = false;
         if(buttonText === this.props.quiz_question.answer){
             this.props.showNextQuestionHandler();
+        } else {
+            incorrectAnswer = true;
         }
+        this.setState({ incorrectAnswer });
     }
 
     render(){
@@ -24,6 +35,7 @@ class QuizQuestion extends Component{
                         )}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer && <p className="error">Sorry, that's not right</p>}
             </main>
         );
     }
